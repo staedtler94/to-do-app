bbdc56d159d5dbe1680d7056d6083f6fcab1be06
Siddhesh Dhage <siddheshdhage@yahoo.com>
2021-04-11 22:10:21 +0100

    Established branch needed quick fix

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
+import router from './router';
 console.log("Todo Output Stage 1")
 
 for(let i=0; i< 10; i++){
 	console.log("Listing Item": i);
 }
+
+console.log(router);
---
+++ router.js
@@ -0,0 +1,7 @@
+
+export function RouteTodo {
+	return {
+	name: "key areas",
+	data: [1, 2, 3]
+	}
+}
